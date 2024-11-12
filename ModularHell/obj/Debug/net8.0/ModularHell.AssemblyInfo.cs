7bce359576fc13805f0e74cd3c539af192afe75b
lukelento06 <lentoluke0@gmail.com>
2024-11-12 15:33:41 -0600

    Added sprite layers to character, added torso implementation for character

--- ModularHell/obj/Debug/net8.0/ModularHell.AssemblyInfo.cs
+++ ModularHell/obj/Debug/net8.0/ModularHell.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ModularHell")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3094a3855725ce6b05d5b81fd9ea130e8516d3c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ca217eaa04e0b8b54ec3a29d2101eb2e702c9af")]
 [assembly: System.Reflection.AssemblyProductAttribute("ModularHell")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ModularHell")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
